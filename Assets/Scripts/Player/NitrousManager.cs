ab13252ab71252567d690e2a9019f0c180dbced9
sneakzttv <57473220+sneakzttv@users.noreply.github.com>
2023-08-07 06:38:57 +0800

    org
    org

--- Assets/Scripts/Player/NitrousManager.cs
+++ Assets/Scripts/Player/NitrousManager.cs
@@ -58,11 +58,14 @@ public class NitrousManager : MonoBehaviour
         // Recharge NOS when it's not in use
         else
         {
+            // Illuminate our NOS Bar
+            BoostBar.color = Color.cyan;
+            NOSIcon.color = Color.cyan;
             currentBoost += rechargeRate * Time.deltaTime;
             currentBoost = Mathf.Clamp(currentBoost, 0f, maxBoost);
-            // Make our bar slightly transparent again
-            BoostBar.color = new Color(1, 1, 1, 0.4f);
-            NOSIcon.color = new Color(1, 1, 1, 0.4f);
+            // Make our bar slightly transparent again (i think this looks better lit up)
+            //BoostBar.color = new Color(1, 1, 1, 0.4f);
+            //NOSIcon.color = new Color(1, 1, 1, 0.4f);
         }
         // Update the value of the UI Slider
         boostSlider.value = currentBoost;
