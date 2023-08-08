d2100d1be9ea198ab8e5cc1a52d3d232349066b2
sneakzttv <57473220+sneakzttv@users.noreply.github.com>
2023-08-08 08:09:23 +0800

    test
    test

--- Assets/Scripts/Player/SpawnPlayer.cs
+++ Assets/Scripts/Player/SpawnPlayer.cs
@@ -41,9 +41,7 @@ public class SpawnPlayer : MonoBehaviour
         {
             // Instantiate our multiplayer prefab
             // i need to change this so it picks a spawn point based on what is taken and whether or not its 1v1 or 5man later
-            //PhotonNetwork.Instantiate(globalManager.playerCar.name, new Vector3(spawnPoints[0].transform.position.x, spawnPoints[0].transform.position.y, spawnPoints[0].transform.position.z), spawnPoints[0].transform.rotation, 0);
-            // DEBUG ------------------------------------------
-            Instantiate(globalManager.playerCar, spawnPoints[0].transform.position, spawnPoints[0].transform.rotation);
+            PhotonNetwork.Instantiate(globalManager.playerCar.name, new Vector3(spawnPoints[0].transform.position.x, spawnPoints[0].transform.position.y, spawnPoints[0].transform.position.z), spawnPoints[0].transform.rotation, 0);
         }
 
         // If we're racing with the Camaro
