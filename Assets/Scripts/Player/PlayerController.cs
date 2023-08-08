17083eafdde68f3a523c12c82941001c24de7aa7
sneakzttv <57473220+sneakzttv@users.noreply.github.com>
2023-08-09 05:40:51 +0800

    yup

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -203,15 +203,16 @@ public class PlayerController : MonoBehaviour
         {
             PVRigidBody.enabled = false;
             PVTransformView.enabled = false;
+            canvas.SetActive(true);
         }
         else
         {
             // disables photon objects that don't belong to us
             if (!PV.IsMine)
             {
-                Destroy(canvas);
-                Destroy(gameObject.GetComponent<PlayerController>());
+                Destroy(this);
             }
+            canvas.SetActive(true);
             userName.SetActive(true);
             userName.GetComponent<TextMesh>().text = globalManager.username;
         }
