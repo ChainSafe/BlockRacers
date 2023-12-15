27582a6584b5287f0c7d5f72e0d8089c88d0fe97
sneakzttv <57473220+sneakzttv@users.noreply.github.com>
2023-12-15 17:02:58 +0800

    2.5.5 integrated

--- Assets/Scripts/Garage/CarSelectionSystem/SwapCars.cs
+++ Assets/Scripts/Garage/CarSelectionSystem/SwapCars.cs
@@ -53,6 +53,8 @@ public class SwapCars : MonoBehaviour
     // Platform
     [SerializeField] private GameObject platform;
 
+    private string[] nftArray;
+
     #endregion
 
     #region Methods
@@ -76,63 +78,42 @@ public class SwapCars : MonoBehaviour
     /// <summary>
     /// Changes to the next car in the list
     /// </summary>
-    public void NextCar()
+    public async void NextCar()
     {
-        switch (nftTypesOwned)
-        {
-            case 0:
-                return;
-            case > 3:
-                nftTypesOwned = 3;
-                break;
-        }
         // Destroy the current prefab
         Destroy(currentPrefab);
-        // Increment the index to switch to the next prefab
         currentPrefabIndex++;
         if (currentPrefabIndex >= nftTypesOwned - 1)
         {
             currentPrefabIndex = 0;
         }
-
+        // NFT stats call based on allnfts arrays using current prefab index
+        var data = await GetStruct(Web3Accessor.Web3, ContractManager.NftContract, ContractManager.NftAbi, "nftStats", new object[] { int.Parse(nftArray[currentPrefabIndex]) });
         // Instantiate the next prefab in the array
-        currentPrefab = Instantiate(prefabs[currentPrefabIndex], spawnPoint, transform.rotation, transform);
-
-        // Logic for handling the car stats and other UI information
-        // Array Index is as follows: 
-        // 0 = Chevrolet Camaro (Nitro Nova GTL)
-        // 1 = Ford GT (Turbo Storm GT)
-        // 2 = Ferrari (Star Stream S6)
-        // Names are subject to change
-
-        switch (currentPrefabIndex)
+        currentPrefab = Instantiate(prefabs[int.Parse(data[0].ToString())], spawnPoint, transform.rotation, transform);
+        // Set name & stats
+        if (data[0] == null) return;
+        switch (data[0])
         {
-            case 0:
-                carName.text = "Nitro Nova GTL";
-                engineSlider.value = globalManager.engineLevel;
-                handlingSlider.value = globalManager.handlingLevel;
-                boostSlider.value = globalManager.nosLevel;
-                // Actively select this car
-                globalManager.playerCar = car1;
-                break;
             case 1:
-                carName.text = "Turbo Storm GT";
-                engineSlider.value = globalManager.engineLevel;
-                handlingSlider.value = globalManager.handlingLevel;
-                boostSlider.value = globalManager.nosLevel;
-                // Actively select this car
-                globalManager.playerCar = car2;
+                globalManager.engineLevelNft1 = int.Parse(data[1].ToString());
+                globalManager.handlingLevelNft1 = int.Parse(data[2].ToString());
+                globalManager.nosLevelNft1 = int.Parse(data[3].ToString());
                 break;
             case 2:
-                carName.text = "Star Stream S6";
-                engineSlider.value = globalManager.engineLevel;
-                handlingSlider.value = globalManager.handlingLevel;
-                boostSlider.value = globalManager.nosLevel;
-                // Actively select this car
-                globalManager.playerCar = car3;
+                globalManager.engineLevelNft2 = int.Parse(data[1].ToString());
+                globalManager.handlingLevelNft2 = int.Parse(data[2].ToString());
+                globalManager.nosLevelNft2 = int.Parse(data[3].ToString());
+                break;
+            case 3:
+                globalManager.engineLevelNft3 = int.Parse(data[1].ToString());
+                globalManager.handlingLevelNft3 = int.Parse(data[2].ToString());
+                globalManager.nosLevelNft3 = int.Parse(data[3].ToString());
                 break;
         }
-
+        engineSlider.value = globalManager.engineLevel;
+        handlingSlider.value = globalManager.handlingLevel;
+        boostSlider.value = globalManager.nosLevel;
         // Play our menu select audio
         GarageMenu.instance.PlayMenuSelect();
     }
@@ -140,78 +121,42 @@ public class SwapCars : MonoBehaviour
     /// <summary>
     /// Changes to the previous car in the list
     /// </summary>
-    public void PreviousCar()
+    public async void PreviousCar()
     {
-        switch (nftTypesOwned)
-        {
-            case 0:
-                return;
-            case > 3:
-                nftTypesOwned = 3;
-                break;
-        }
-
         // Destroy the current prefab
         Destroy(currentPrefab);
-        // Decrement the index to switch to the next prefab  
-        if (currentPrefabIndex > 0)
+        currentPrefabIndex--;
+        if (currentPrefabIndex <= nftTypesOwned)
         {
-            currentPrefabIndex--;
+            currentPrefabIndex = nftTypesOwned -1;
         }
-        else
-        {
-            currentPrefabIndex = nftTypesOwned - 1;
-        }
-
+        // NFT stats call based on allnfts arrays using current prefab index
+        var data = await GetStruct(Web3Accessor.Web3, ContractManager.NftContract, ContractManager.NftAbi, "nftStats", new object[] { int.Parse(nftArray[currentPrefabIndex]) });
         // Instantiate the next prefab in the array
-        currentPrefab = Instantiate(prefabs[currentPrefabIndex], spawnPoint, transform.rotation, transform);
-
-        // Logic for handling the car stats and other UI information
-        // Array Index is as follows: 
-        // 0 = Chevrolet Camaro (Nitro Nova GTL)
-        // 1 = Ford GT (Turbo Storm GT)
-        // 2 = Ferrari (Star Stream S6)
-        // Names are subject to change
-
-        switch (currentPrefabIndex)
+        currentPrefab = Instantiate(prefabs[int.Parse(data[0].ToString())], spawnPoint, transform.rotation, transform);
+        // Set name & stats
+        if (data[0] == null) return;
+        switch (data[0])
         {
-            case 0:
-                carName.text = "Nitro Nova GTL";
-                engineSlider.value = globalManager.engineLevelNft1;
-                handlingSlider.value = globalManager.handlingLevelNft1;
-                boostSlider.value = globalManager.nosLevelNft1;
-                // Actively select this car
-                globalManager.playerCar = car1;
-                globalManager.engineLevel = globalManager.engineLevelNft1;
-                globalManager.handlingLevel = globalManager.handlingLevelNft1;
-                globalManager.nosLevel = globalManager.nosLevelNft1;
-                break;
-
             case 1:
-                carName.text = "Turbo Storm GT";
-                engineSlider.value = globalManager.engineLevelNft2;
-                handlingSlider.value = globalManager.handlingLevelNft2;
-                boostSlider.value = globalManager.nosLevelNft2;
-                // Actively select this car
-                globalManager.playerCar = car2;
-                globalManager.engineLevel = globalManager.engineLevelNft2;
-                globalManager.handlingLevel = globalManager.handlingLevelNft2;
-                globalManager.nosLevel = globalManager.nosLevelNft2;
+                globalManager.engineLevelNft1 = int.Parse(data[1].ToString());
+                globalManager.handlingLevelNft1 = int.Parse(data[2].ToString());
+                globalManager.nosLevelNft1 = int.Parse(data[3].ToString());
                 break;
-
             case 2:
-                carName.text = "Star Stream S6";
-                engineSlider.value = globalManager.engineLevelNft3;
-                handlingSlider.value = globalManager.handlingLevelNft3;
-                boostSlider.value = globalManager.nosLevelNft3;
-                // Actively select this car
-                globalManager.playerCar = car3;
-                globalManager.engineLevel = globalManager.engineLevelNft3;
-                globalManager.handlingLevel = globalManager.handlingLevelNft3;
-                globalManager.nosLevel = globalManager.nosLevelNft3;
+                globalManager.engineLevelNft2 = int.Parse(data[1].ToString());
+                globalManager.handlingLevelNft2 = int.Parse(data[2].ToString());
+                globalManager.nosLevelNft2 = int.Parse(data[3].ToString());
+                break;
+            case 3:
+                globalManager.engineLevelNft3 = int.Parse(data[1].ToString());
+                globalManager.handlingLevelNft3 = int.Parse(data[2].ToString());
+                globalManager.nosLevelNft3 = int.Parse(data[3].ToString());
                 break;
         }
-
+        engineSlider.value = globalManager.engineLevel;
+        handlingSlider.value = globalManager.handlingLevel;
+        boostSlider.value = globalManager.nosLevel;
         // Play our menu select audio
         GarageMenu.instance.PlayMenuSelect();
     }
@@ -287,12 +232,11 @@ public class SwapCars : MonoBehaviour
         var account = await Web3Accessor.Web3.Signer.GetAddress();
         var data = await GetArray(Web3Accessor.Web3, ContractManager.NftContract, ContractManager.NftAbi, "getOwnerNftIds", new object[] {account});
         var response = string.Join(" ", data.Select(list => string.Join(" ", list)));
-        var resultArray = response.Split(' ');
+        nftArray = response.Split(' ');
         if (response == "") return;
         Debug.Log($"Result: {response}");
-        foreach (var nftId in resultArray)
+        foreach (var nftId in nftArray)
         {
-            Debug.Log("ADDING NFT " + nftId);
             nftTypesOwned++;
             GetNftStats(int.Parse(nftId));
         }
