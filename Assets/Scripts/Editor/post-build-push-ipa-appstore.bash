bbcf86fda5cdfcfc7c553ee34027f4dfcb316042
Nikola Garabandic <kantagara@gmail.com>
2024-02-09 15:30:02 +0100

    Another potential fix.

--- Assets/Scripts/Editor/post-build-push-ipa-appstore.bash
+++ Assets/Scripts/Editor/post-build-push-ipa-appstore.bash
@@ -2,9 +2,9 @@
 echo "Uploading IPA to Appstore Connect..."
  
 #Path is "/BUILD_PATH/<ORG_ID>.<PROJECT_ID>.<BUILD_TARGET_ID>/.build/last/<BUILD_TARGET_ID>/build.ipa"
-path="/BUILD_PATH/kantagara.block-racers.block-racers-ios/.build/last/block-racers-ios/build.ipa"
- 
-if xcrun altool --upload-app -f $path -u $ITUNES_USERNAME -p $ITUNES_PASSWORD ; then
+
+
+if xcrun altool --upload-app -f "/BUILD_PATH/kantagara.block-racers.block-racers-ios/.build/last/block-racers-ios/build.ipa" --type ios -u $ITUNES_USERNAME -p $ITUNES_PASSWORD ; then
     echo "Upload IPA to Appstore Connect finished with success"
 else
     echo "Upload IPA to Appstore Connect failed"
