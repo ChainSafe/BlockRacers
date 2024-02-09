ee91cf4af87b1605f499cc2ec838546f36734f40
Nikola Garabandic <kantagara@gmail.com>
2024-02-09 19:30:47 +0100

    Fix

--- Assets/Scripts/Editor/IncrementBuildNumberAndAddLinkerFlag.cs
+++ Assets/Scripts/Editor/IncrementBuildNumberAndAddLinkerFlag.cs
@@ -29,7 +29,7 @@ public class IncrementBuildNumberAndAddLinkerFlag : IPostprocessBuildWithReport,
 
         // Set a custom link flag
         proj.AddBuildProperty(target, "OTHER_LDFLAGS", "-ld_classic");
-        proj.SetBuildProperty(targetGuid, "ALWAYS_EMBED_SWIFT_STANDARD_LIBRARIES", "YES");
+        proj.SetBuildProperty(target, "ALWAYS_EMBED_SWIFT_STANDARD_LIBRARIES", "YES");
 
 
         File.WriteAllText(projPath, proj.WriteToString());
