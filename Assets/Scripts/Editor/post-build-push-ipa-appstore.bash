21cfefcef42ea153c6b5119b9313423ffad8c45f
Nikola Garabandic <kantagara@gmail.com>
2024-02-09 16:33:26 +0100

    ios type

--- Assets/Scripts/Editor/post-build-push-ipa-appstore.bash
+++ Assets/Scripts/Editor/post-build-push-ipa-appstore.bash
@@ -4,7 +4,7 @@ echo "Uploading IPA to Appstore Connect..."
 #Path is "/BUILD_PATH/<ORG_ID>.<PROJECT_ID>.<BUILD_TARGET_ID>/.build/last/<BUILD_TARGET_ID>/build.ipa"
 path="$WORKSPACE/.build/last/block-racers-ios/build.ipa"
  
-if xcrun altool --upload-app -f $path -u $ITUNES_USERNAME -p $ITUNES_PASSWORD ; then
+if xcrun altool --upload-app -f $path -u $ITUNES_USERNAME -p $ITUNES_PASSWORD --type ios ; then
     echo "Upload IPA to Appstore Connect finished with success"
 else
     echo "Upload IPA to Appstore Connect failed"
