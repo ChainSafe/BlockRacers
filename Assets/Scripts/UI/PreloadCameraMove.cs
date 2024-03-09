f9429f05fecbd425813bc8822b84fb7a247de728
sneakzttv <57473220+sneakzttv@users.noreply.github.com>
2024-03-10 02:53:38 +0800

    Preload camera wallet fix
    Made the camera be there for  a few seconds to distract the user whilst wallet config loads

---
+++ Assets/Scripts/UI/PreloadCameraMove.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+
+public class PreloadCameraMove : MonoBehaviour
+{
+    public Transform objectToMove;
+    public float targetX = -170;
+    public float duration = 3f;
+
+    private Vector3 startPosition;
+    private Vector3 targetPosition;
+    private float startTime;
+
+    void Start()
+    {
+        startPosition = objectToMove.position;
+        targetPosition = new Vector3(targetX, startPosition.y, startPosition.z);
+        startTime = Time.time;
+    }
+
+    void Update()
+    {
+        float timeSinceStarted = Time.time - startTime;
+        float percentageComplete = timeSinceStarted / duration;
+
+        objectToMove.position = Vector3.Lerp(startPosition, targetPosition, percentageComplete);
+
+        if (percentageComplete >= 1.0f)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
