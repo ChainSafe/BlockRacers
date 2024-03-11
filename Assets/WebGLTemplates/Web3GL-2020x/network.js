06c852f1ae2530d5e8243e909618a83201627ad6
sneakzttv <57473220+sneakzttv@users.noreply.github.com>
2024-03-11 19:52:15 +0800

    rpc + camera fix
    rpc + camera fix

--- Assets/WebGLTemplates/Web3GL-2020x/network.js
+++ Assets/WebGLTemplates/Web3GL-2020x/network.js
@@ -4,6 +4,6 @@ window.networks = [
     id: 338,
     label: "Cronos Testnet CRO",
     token: "TCRO",
-    rpcUrl: 'https://evm-t3.cronos.org',
+    rpcUrl: 'https://nd-112-751-357.p2pify.com/9c867889890df4708e902e89eab1370d',
   }
 ]
